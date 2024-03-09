273b811f7f8a611de8107912b8f1ad73e798981d
harukikawakatsu <ks22124kh@tcue.ac.jp>
2024-03-09 15:57:44 +0900

    データ取得制限機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,7 +17,8 @@ class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
         {
-            return view('posts.index')->with(['posts' => $post->get()]);
+            return view('posts.index')->with(['posts' => $post->getByLimit()]);
              //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+             //getbylimitでデータ取得数を制限出来るようにしている。
         }
 }
